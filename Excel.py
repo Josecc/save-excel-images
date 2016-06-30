9b7eb9bb69ba6dd21c367c3ddf96e8a6de54294b
Jose Canahui <jose.canahui@gmail.com>
2016-06-29 23:57:18 -0400

    removed sheet name

--- Excel.py
+++ Excel.py
@@ -9,7 +9,7 @@ class Excel():
         self.fileName = self.__getFile(fileName)
 
     def read(self, columnName=None):
-        df = pd.read_excel(self.fileName, "PatentImageTest")
+        df = pd.read_excel(self.fileName)
         if columnName is None:
             imageURLs = df.as_matrix()
         else:
